6f7846a4105926f57678c53b384b5a2e5fbafe74
theonediepplayer <105558405+theonediepplayer@users.noreply.github.com>
2022-05-14 20:24:44 +0300

    Update notification.user.js

--- userscripts/notification.user.js
+++ userscripts/notification.user.js
@@ -8,8 +8,8 @@
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=herokuapp.com
 // @grant        none
 // ==/UserScript==
-if (!window.Hook) return;
-if (!window.Hook) alert('You need Diep.io Packet WASM Hook to use this')
+if (!window.Hook) return window.alert('You need Diep.io Packet WASM Hook to use this script.');
+
 document.addEventListener('keydown', function (event) {
     if (event.key == 'n') {
       Hook.recv(new Uint8Array([3, ...new TextEncoder().encode(`notification :D`), 0, 0, 0, 255, 0, ...new Uint8Array(new Float32Array([2500]).buffer), 65, 0]));
