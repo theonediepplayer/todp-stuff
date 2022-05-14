9473630a770f3a22fa270c758374ea716c91381f
theonediepplayer <105558405+theonediepplayer@users.noreply.github.com>
2022-05-14 19:42:26 +0300

    Update partycode.user.js

--- userscripts/partycode.user.js
+++ userscripts/partycode.user.js
@@ -1,1 +1,18 @@
-
+// ==UserScript==
+// @name         Copy party link
+// @namespace    http://tampermonkey.net/
+// @version      0.9
+// @description  allows u to copy links from ffa, tag, survival, maze AND 2 TEAMS (press [), you need wasm hook, get it from https://github.com/ABCxFF/diepindepth/blob/main/protocol/userscripts/packethook.user.js
+// @author       todp
+// @match        https://diep.io/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=diep.io
+// @grant        none
+// ==/UserScript==
+function recv(args) {
+Hook.recv(new Uint8Array([args]))
+}
+ document.addEventListener("keydown", (kc) => {
+        if (kc.keyCode === 219) {
+            recv(6);
+        }
+    });
